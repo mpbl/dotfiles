026a23b6961514c4be552a53b25afdb1b17fb016
Dominik Drexl <domdrexl@gmail.com>
2017-05-14 01:08:56 +0200

    Extended install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,17 @@
+#!/usr/bin/env bash
 # install some basic command line utilities using apt
-sudo apt install \
-    vim \
-    git \
-    tree
 
+packages=(
+vim
+git
+tree
+cmake
+build-essential
+curl
+rsync
+)
+
+sudo apt update
+echo ${packages[*]} | xargs sudo apt install --assume-yes
+
+unset packages;
